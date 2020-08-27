851bc1fffe2457993ba7489657430bca92a8751a
Calum Smith <hello@calum.io>
2020-08-27 12:57:49 -0400

    Update typescript plugin interface definition to match reality
    Rearrange types to reflect platform inconsistencies, and remove type assertions from web.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,9 @@
-import { Authentication, User } from "./user";
+import { WebAuthentication, iOSAuthentication, User } from "./user";
 
 export interface GoogleAuthPlugin {
   signIn(): Promise<User>;
-  refresh(): Promise<Authentication>;
-  signOut(): Promise<any>;
+  refresh(): Promise<WebAuthentication | iOSAuthentication>;
+  signOut(): Promise<void>;
 
   /** Init hook for load gapi and init plugin */
   init(): void;
--- src/user.ts
+++ src/user.ts
@@ -1,17 +1,39 @@
-export interface User {
+interface CommonUser {
     id: string;
     email: string;
     
     name: string;
     familyName: string;
     givenName: string;
-    imageUrl: string;
+    imageUrl?: string;
 
     serverAuthCode: string;
+}
+
+export interface WebUser extends CommonUser {
+    authentication: WebAuthentication;
+}
+
+interface iOSUser extends CommonUser {
+    authentication: iOSAuthentication;
+}
+
+interface AndroidUser extends CommonUser {
+    idToken: string;
     authentication: Authentication;
 }
 
+export type User = WebUser | iOSUser | AndroidUser;
+
 export interface Authentication {
-    accessToken: string;
     idToken: string;
+}
+
+export interface WebAuthentication extends Authentication {
+    accessToken: string;
+}
+
+export interface iOSAuthentication extends Authentication {
+    accessToken: string;
+    refreshToken: string;
 }
